bb9ab81e0ad5c7eddd40eb9dcbade24cbffe1b25
William Reticena <william.wsd43@gmail.com>
2023-01-11 20:19:13 -0300

    feat: expansion-item created

--- src/components/library/TabPanel/helpers/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentPublicInstance } from 'vue'
-
-export interface ParentProps extends ComponentPublicInstance {
-  modelValue: null | string
-}
-
-export interface AttrProps {
-  name?: string
-}
