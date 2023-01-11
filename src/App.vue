8b086f8bd190d220806ba7707c68f9bb34151525
William Reticena <william.wsd43@gmail.com>
2023-01-11 19:03:14 -0300

    style: add global styles on app component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,25 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return {};
+    return {}
   },
-});
+})
 </script>
 
-<style scoped>
+<style>
+:root {
+  --primary-color: #1976d2;
+  --secondary-color: #26a69a;
+
+  font-size: 14px;
+}
+
+* {
+  box-sizing: border-box;
+  font-size: 1rem;
+}
 </style>
