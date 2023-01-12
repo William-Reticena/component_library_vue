36f65d2069a1cc984d26d358bad7107963995d70
William Reticena <wsreticena@viptech.com.br>
2023-01-12 11:39:30 -0300

    chore: remove unused classes

--- src/components/library/Modal/index.vue
+++ src/components/library/Modal/index.vue
@@ -5,18 +5,7 @@
         <div ref="modalRef" class="modal" :class="{ [`modal--${variant}`]: variant }" :style="{ height, width }" tabindex="-1">
           <div v-if="!hideIcon" class="modal-close-button-container">
             <button class="modal-close-button" @click="!preventClosing ? handleClose() : null">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                class="feather feather-x"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <line x1="18" y1="6" x2="6" y2="18"></line>
                 <line x1="6" y1="6" x2="18" y2="18"></line>
               </svg>
